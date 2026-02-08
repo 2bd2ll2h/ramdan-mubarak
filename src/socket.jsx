f945002bcd85a6849107ff46c85213954ce24ebb
2bd2ll2h <166227742+2bd2ll2h@users.noreply.github.com>
2026-02-09 00:23:24 +0200

    Add files via upload

---
+++ src/socket.jsx
@@ -0,0 +1,49 @@
+// src/socket.js
+import { io } from "socket.io-client";
+export 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+// بدلاً من io("http://localhost:3001")
+const socket = io("https://asset-manager--bdallahashrf110.replit.app");
+
+
+
+
+
+
+
+
+
+
+
+
