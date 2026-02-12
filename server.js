e2bdf30910840f9f29c3e1912de852bf3be42a9f
2bd2ll2h <166227742+2bd2ll2h@users.noreply.github.com>
2026-02-12 20:43:54 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 const express = require("express");
-const http = r
+const http = require("http");
+const { Server } = require("socket.io");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { 
+    cors: { 
+        origin: "*", // ده بيسمح لأي موقع (زي فيرسل) يكلم السيرفر
+        methods: ["GET", "POST"]
+    } 
+});
+
+app.use(cors());
+app.use(express.json());
+
 // التأكد من وجود مجلد الرفع
 const UPLOAD_DIR = path.join(__dirname, "uploads");
 
@@ -187,5 +205,4 @@ app.get("/images", (_, res) => res.json(savedChallenges));
 const PORT = process.env.PORT || 5000; 
 server.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
-
 });
